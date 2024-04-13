bef450ea8e787516ac9f9a06c9d2c7e437665af2
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2024-04-13 19:04:18 +0330

    Try Using react components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
+import { func } from 'prop-types';
 import './style.css';
 
 function App() {
 	return (
+		<>
+			<Header />
+			<NewFactForm />
+
+			<main className='main'>
+        <CategoryFilter />
+        <FactList />
+      </main>
+		</>
+	);
+}
+
+function Header() {
+	const appTitle = 'Today I Learned';
+
+	return (
 		<header className='header'>
 			<div className='logo'>
 				<img src='logo.png' height='68' width='68' alt='Today I Learned Logo' />
-				<h1>Today I Learned</h1>
+				<h1>{appTitle}</h1>
 			</div>
 
 			<button className='btn btn-large btn-open'>Share a fact</button>
 		</header>
 	);
+}
+
+function NewFactForm() {
+	return <form className='fact-form'>Fact Form</form>;
+}
+
+function CategoryFilter() {
+	return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Fact List</section>;
 }
 
 export default App;
