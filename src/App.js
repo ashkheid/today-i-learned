fdaea1a177b60c9fc103948bfadda24d1ff7aba7
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2024-05-02 21:52:40 +0330

    Add: form template

--- src/App.js
+++ src/App.js
@@ -85,7 +85,20 @@ function Header({ appTitle, showFrom, setShowForm }) {
 }
 
 function NewFactForm() {
-	return <form className='fact-form'>Fact Form</form>;
+	return (
+		<form className='fact-form'>
+			<input type='text' placeholder='Share a fact with the world...' />
+			<span>200</span>
+			<input type='text' placeholder='Trustworthy source...' />
+			<select>
+				<option value=''>Choose category:</option>
+				<option value='technology'>Technology</option>
+				<option value='science'>Science</option>
+				<option value='finance'>Finance</option>
+			</select>
+			<button class='btn btn-large'>Post</button>
+		</form>
+	);
 }
 
 function CategoryFilter() {
