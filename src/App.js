6144768007eeff19539b0290a54674cddcc2ef2e
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2024-05-02 21:01:21 +0330

    Create CategoryFilter buttons

--- src/App.js
+++ src/App.js
@@ -83,13 +83,20 @@ function CategoryFilter() {
 		<aside>
 			<ul>
 				<li className='category'>
-					<button
-						className='btn btn-category'
-						style={{ backgroundColor: '#3b82f6' }}
-					>
-						Technology
-					</button>
+					<button className='btn btn-all-categories'>All</button>
 				</li>
+				{CATEGORIES.map((cat) => {
+					return (
+						<li key={cat.name} className='category'>
+							<button
+								className='btn btn-category'
+								style={{ backgroundColor: cat.color }}
+							>
+								{cat.name}
+							</button>
+						</li>
+					);
+				})}
 			</ul>
 		</aside>
 	);
